0ae0cf651b0b575d8f2e7c3b3b781ad075ed5539
ShayEarl <15810203606@163.com>
2020-11-29 22:07:59 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import Page1 from "./component/Page1"
 
 function App() {
   return (
     <div className="App">
      未知知识补充
+     <div>
+       <Page1/>
+     </div>
     </div>
   );
 }
---
+++ src/component/Page1.jsx
@@ -0,0 +1,9 @@
+import {Button} from "antd"
+export default function Page1(){
+    return (
+        <div>
+            <Button type="default">sdfa</Button>
+            
+        </div>
+    )
+}
