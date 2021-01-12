e67f24fb899e37cc893ec34af7178b3129c9058d
ShayEarl <15810203606@163.com>
2021-01-12 21:24:02 +0800

    add
    add

--- src/page/reduxModule/HGRedux-dome/HGRedux/applyMiddleware.js
+++ src/page/reduxModule/HGRedux-dome/HGRedux/applyMiddleware.js
@@ -1,36 +1,36 @@
 // const { applyMiddleware } = require("redux");
 
-export default function HGapplyMiddleware(...Middlewares){
-    return (createStore)=> reducer=>{
-        const store=createStore(reducer);
-        let dispatch=store.dispatch
-        // let getState=store.getState;
-        let modApi={
-            getState:store.getState,
-            dispatch:(action,...arg)=>dispatch(action,arg)
-        }
-        const MiddlewareChain=Middlewares.map(Middleware=>Middleware(modApi));
-        dispatch=compose(...MiddlewareChain)(store.dispatch)
-        //加强dispatch
-        return {
-        ...store,
-        dispatch,
-        }
+export default function HGapplyMiddleware(...Middlewares) {
+  return (createStore) => reducer => {
+    const store = createStore(reducer);
+    let dispatch = store.dispatch
+    // let getState=store.getState;
+    let modApi = {
+      getState: store.getState,
+      dispatch: (action, ...arg) => dispatch(action, arg)
     }
+    const MiddlewareChain = Middlewares.map(Middleware => Middleware(modApi));
+    dispatch = compose(...MiddlewareChain)(store.dispatch)
+    console.log(dispatch)
+    //加强dispatch
+    return {
+      ...store,
+      dispatch,
+    }
+  }
 }
 
 
 function compose(...funcs) {
-    if (funcs.length === 0) {
-      // 返回一个函数
-      return arg => arg;
-    }
-  
-    if (funcs.length === 1) {
-      return funcs[0];
-    }
-  
-    // 返回了一个聚合函数
-    return funcs.reduce((a, b) => (...args) => a(b(...args)));
+  if (funcs.length === 0) {
+    // 返回一个函数
+    return arg => arg;
   }
-  
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  // 返回了一个聚合函数
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
+}
--- src/page/reduxModule/HGRedux-dome/store/index.js
+++ src/page/reduxModule/HGRedux-dome/store/index.js
@@ -1,7 +1,7 @@
 import {createStore,HGapplyMiddleware} from "../HGRedux";
 // import {createStore} from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 function countReducer(state = 0, action) {
   switch (action.type) {
     case "ADD":
@@ -13,6 +13,34 @@ function countReducer(state = 0, action) {
   }
 }
 
-const store = createStore(countReducer,HGapplyMiddleware(thunk,logger));
+const store = createStore(countReducer,HGapplyMiddleware(thunk, logger));
+function logger({getState}){
+  return  next=>action=>{
+      console.log("--------------------- sgy-log start")
+      let prevState=getState();
+      console.log("prev state",prevState)
+      const returnValue=next(action)
+      let nextState=getState();
+      console.log("next state",nextState)
+      console.log("--------------------- sgy-log end")
+      return returnValue;
+  }
+}
+// function logger({getState}) {
+//   return next => action => {
+//     console.log("-------------------------------"); //sy-log
+
+//     let prevState = getState();
+//     console.log("prev state", prevState); //sy-log
+
+//     const returnValue = next(action);
+
+//     let nextState = getState();
+//     console.log("next state", nextState); //sy-log
+
+//     console.log("-------------------------------"); //sy-log
+//     return returnValue;
+//   };
+// }
 
 export default store;
